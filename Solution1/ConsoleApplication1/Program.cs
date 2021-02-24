451cb69f26dc3e24e6e6de631d189c60c2d51a64
Nour <690353945@qq.com>
2021-02-25 00:00:09 +0800

    this is my first c# solution

---
+++ Solution1/ConsoleApplication1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+           //实现八皇后问题
+        }
+
+
+        public bool judge(int n, int[] arr)
+        {
+            
+        }
+    }
+}
